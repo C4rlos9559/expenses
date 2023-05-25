0b1f063b3dc62d582c68b758f76866fd80b04d80
Carlos Diaz <carlos9559@gmail.com>
2023-05-25 14:38:33 -0500

    Interfaz con bootstrap

--- src/App.js
+++ src/App.js
@@ -1,25 +1,123 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import React, { Component } from "react";
+import "./App.css";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import Stack from "react-bootstrap/Stack";
+import ListGroup from "react-bootstrap/ListGroup";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { InputGroup } from "react-bootstrap";
+import "./css/styles.css";
 
-function App() {
+const Header = () => {
+  return <h1>Budget planner</h1>;
+};
+const Expenses = ({ elements, deleteItem }) => {
+  if (elements.length > 0) {
+    return (
+      <div id="ul-list">
+        <ListGroup id="ul-items">
+          {elements.map((element, index) => (
+            <Expense element={element} index={index} deleteItem={deleteItem} />
+          ))}
+        </ListGroup>
+      </div>
+    );
+  }
+};
+const Expense = ({ element, index, deleteItem }) => {
+  console.log(index);
+  const onDelete = () => {
+    deleteItem(index);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <React.Fragment>
+      <ListGroup.Item id="li-container">
+        <Stack direction="horizontal" id="li-items" gap={2}>
+          <ListGroup horizontal id="item-content">
+            <ListGroup.Item as="li" id="item-text">
+              {element.item}
+            </ListGroup.Item>
+            <ListGroup.Item as="li" id="item-price">
+              ${element.price}
+            </ListGroup.Item>
+          </ListGroup>
+          <Button variant="secondary" id="delete-item" onClick={onDelete}>
+            Delete
+          </Button>
+        </Stack>
+      </ListGroup.Item>
+    </React.Fragment>
+  );
+};
+const FormInput = ({ onAdd }) => {
+  const [item, setItem] = React.useState("");
+  const [price, setPrice] = React.useState("");
+  const onChangeItem = (event) => {
+    setItem(event.target.value);
+  };
+  const onChangePrice = (event) => {
+    setPrice(event.target.value);
+  };
+  async function addElement(event) {
+    event.preventDefault();
+    if (item.trim() !== "" && price > 0) {
+      onAdd({ item, price });
+      setItem("");
+      setPrice("");
+    } else {
+      alert("Elemento no válido");
+    }
+  }
+
+  return (
+    <form id="form-Input">
+      <Stack direction="horizontal" id="form-elements">
+        <Form.Control
+          id="item-input"
+          type="text"
+          onChange={onChangeItem}
+          placeholder="Add an item"
+          value={item}
+        />
+        <InputGroup id="price-input">
+          <InputGroup.Text>$</InputGroup.Text>
+          <Form.Control
+            name="price-input"
+            type="number"
+            onChange={onChangePrice}
+            placeholder="Add a price"
+            value={price}
+          />
+        </InputGroup>
+
+        <Button variant="primary" onClick={addElement} id="add-input">
+          Add
+        </Button>
+      </Stack>
+      <br />
+    </form>
+  );
+};
+const App = () => {
+  const [items, setItems] = React.useState([]);
+  const deleteItem = (index) => {
+    let temp = [...items];
+    temp.splice(index, 1);
+    setItems(temp);
+  };
+  const onAdd = (element) => {
+    setItems([...items, element]);
+  };
+  return (
+    <div id="main-container">
+      <React.Fragment>
+        <Header />
+        <FormInput onAdd={onAdd} />
+        <br />
+        <Expenses elements={items} deleteItem={deleteItem} />
+      </React.Fragment>
     </div>
   );
-}
-
+};
 export default App;
