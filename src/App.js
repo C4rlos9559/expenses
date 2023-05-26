a1d4c02f14d900979d98d3d6ea40ec3178dd4649
Carlos Diaz <carlos9559@gmail.com>
2023-05-25 21:23:56 -0500

    Update-Save, Delete-Cancel

--- src/App.js
+++ src/App.js
@@ -8,27 +8,52 @@ import ListGroup from "react-bootstrap/ListGroup";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { InputGroup } from "react-bootstrap";
 import "./css/styles.css";
+import ButtonGroup from "react-bootstrap/ButtonGroup";
 
 const Header = () => {
   return <h1>Budget planner</h1>;
 };
-const Expenses = ({ elements, deleteItem }) => {
+const Expenses = ({ elements, deleteItem, updateItem }) => {
   if (elements.length > 0) {
     return (
-      <div id="ul-list">
+      <div class="ul-list">
         <ListGroup id="ul-items">
           {elements.map((element, index) => (
-            <Expense element={element} index={index} deleteItem={deleteItem} />
+            <Expense
+              element={element}
+              index={index}
+              deleteItem={deleteItem}
+              updateItem={updateItem}
+            />
           ))}
         </ListGroup>
       </div>
     );
   }
 };
-const Expense = ({ element, index, deleteItem }) => {
-  console.log(index);
+const Expense = ({ element, index, deleteItem, updateItem }) => {
+  const [reading, setReading] = React.useState(true);
+  const [editItemValue, setEditItemValue] = React.useState(element.item);
+  const [editItemPrice, setEditItemPrice] = React.useState(element.price);
   const onDelete = () => {
-    deleteItem(index);
+    reading ? deleteItem(index) : setReading(!reading);
+  };
+  const onUpdate = () => {
+    if (reading) {
+      setReading(!reading);
+      setEditItemValue(element.item);
+      setEditItemPrice(element.price);
+    } else {
+      console.log(editItemValue + " -> " + editItemPrice);
+      updateItem(index, editItemValue, editItemPrice);
+      setReading(!reading);
+    }
+  };
+  const onChangeItemValue = (event) => {
+    setEditItemValue(event.target.value);
+  };
+  const onChangeItemPrice = (event) => {
+    setEditItemPrice(event.target.value);
   };
   return (
     <React.Fragment>
@@ -36,15 +61,41 @@ const Expense = ({ element, index, deleteItem }) => {
         <Stack direction="horizontal" id="li-items" gap={2}>
           <ListGroup horizontal id="item-content">
             <ListGroup.Item as="li" id="item-text">
-              {element.item}
+              {reading ? (
+                element.item
+              ) : (
+                <Form.Control
+                  id="item-value"
+                  type="text"
+                  onChange={onChangeItemValue}
+                  placeholder={element.item}
+                  value={editItemValue}
+                />
+              )}
             </ListGroup.Item>
             <ListGroup.Item as="li" id="item-price">
-              ${element.price}
+              {reading ? (
+                `$ ${element.price}`
+              ) : (
+                <Form.Control
+                  type="text"
+                  onChange={onChangeItemPrice}
+                  placeholder={element.price}
+                  value={editItemPrice}
+                />
+              )}
             </ListGroup.Item>
           </ListGroup>
-          <Button variant="secondary" id="delete-item" onClick={onDelete}>
-            Delete
-          </Button>
+          <div class="btn-options">
+            <ButtonGroup aria-label="options">
+              <Button variant="success" id="update-item" onClick={onUpdate}>
+                {reading ? "Update" : "Save"}
+              </Button>
+              <Button variant="danger" id="delete-item" onClick={onDelete}>
+                {reading ? "Delete" : "Cancel"}
+              </Button>
+            </ButtonGroup>
+          </div>
         </Stack>
       </ListGroup.Item>
     </React.Fragment>
@@ -63,13 +114,12 @@ const FormInput = ({ onAdd }) => {
     event.preventDefault();
     if (item.trim() !== "" && price > 0) {
       onAdd({ item, price });
-      setItem("");
-      setPrice("");
+      setItem(""); //Clear field
+      setPrice(""); //Clear field
     } else {
       alert("Elemento no válido");
     }
   }
-
   return (
     <form id="form-Input">
       <Stack direction="horizontal" id="form-elements">
@@ -90,7 +140,6 @@ const FormInput = ({ onAdd }) => {
             value={price}
           />
         </InputGroup>
-
         <Button variant="primary" onClick={addElement} id="add-input">
           Add
         </Button>
@@ -106,6 +155,12 @@ const App = () => {
     temp.splice(index, 1);
     setItems(temp);
   };
+  const updateItem = (index, newItem, newPrice) => {
+    let temp = [...items];
+    temp[index].item = newItem;
+    temp[index].price = newPrice;
+    setItems(temp);
+  };
   const onAdd = (element) => {
     setItems([...items, element]);
   };
@@ -115,7 +170,11 @@ const App = () => {
         <Header />
         <FormInput onAdd={onAdd} />
         <br />
-        <Expenses elements={items} deleteItem={deleteItem} />
+        <Expenses
+          elements={items}
+          deleteItem={deleteItem}
+          updateItem={updateItem}
+        />
       </React.Fragment>
     </div>
   );
